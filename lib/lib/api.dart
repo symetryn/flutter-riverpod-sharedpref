0e2433ba92c32299815461c45ee48921a41ba97b
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-08-09 17:06:24 +0545

    feat: updated api instance taking config

--- lib/lib/api.dart
+++ lib/lib/api.dart
@@ -2,37 +2,52 @@ library api;
 
 import 'package:dio/dio.dart';
 import 'package:flutter_skeleton/config.dart';
+import 'package:flutter_skeleton/enums/env.dart';
 
-var options = BaseOptions(
-  baseUrl: Config.BASE_URL,
-  connectTimeout: 5000,
-  receiveTimeout: 3000,
-);
+import '../config.dart';
 
-void _requestInterceptor(
-  RequestOptions options,
-  RequestInterceptorHandler handler,
-) async {
-  handler.next(options);
+class HttpClient {
+  static final BaseOptions options = BaseOptions(
+    baseUrl: Config.BASE_URL,
+    connectTimeout: 5000,
+    receiveTimeout: 3000,
+  );
+
+  final Dio httpClient = Dio(options);
+
+  HttpClient() {
+    this.httpClient.interceptors.add(
+          InterceptorsWrapper(
+            onError: _errorInterceptor,
+            onRequest: _requestInterceptor,
+            onResponse: _responseInterceptor,
+          ),
+        );
+    if (Config.ENV == AppEnv.local) {
+      this.httpClient.interceptors.add(LogInterceptor());
+    }
+  }
+
+  void _requestInterceptor(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    handler.next(options);
+  }
+
+  void _responseInterceptor(
+    Response<dynamic> response,
+    ResponseInterceptorHandler handler,
+  ) async {
+    handler.next(response);
+  }
+
+  void _errorInterceptor(
+    DioError err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    handler.next(err);
+  }
 }
 
-void _responseInterceptor(
-  Response<dynamic> response,
-  ResponseInterceptorHandler handler,
-) async {
-  handler.next(response);
-}
-
-void _errorInterceptor(
-  DioError err,
-  ErrorInterceptorHandler handler,
-) async {
-  handler.next(err);
-}
-
-Dio httpClient = Dio(options)
-  ..interceptors.add(InterceptorsWrapper(
-    onRequest: _requestInterceptor,
-    onResponse: _responseInterceptor,
-    onError: _errorInterceptor,
-  ));
+var httpClient = HttpClient().httpClient;
